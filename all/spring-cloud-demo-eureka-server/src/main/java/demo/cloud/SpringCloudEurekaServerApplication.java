fe7957e2f3b9be5b9cb8765716ae36d645887f73
leishiguang <leishiguang@outlook.com>
2019-09-09 00:24:55 +0800

    add eureka server application

---
+++ all/spring-cloud-demo-eureka-server/src/main/java/demo/cloud/SpringCloudEurekaServerApplication.java
@@ -0,0 +1,21 @@
+package demo.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 启动 Eureka Server 服务
+ *
+ * @author leishiguang
+ * @since v1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaServerApplication.class, args);
+    }
+
+}
