1537734777800aa651fe68877b224fd74a29ac35
leishiguang <leishiguang@outlook.com>
2019-12-17 00:22:06 +0800

    完成 graylog 日志集成

--- spring-boot-demo/spring-boot-demo-graylog/src/main/java/demo/boot/graylog/BootGraylogApplication.java
+++ spring-boot-demo/spring-boot-demo-graylog/src/main/java/demo/boot/graylog/BootGraylogApplication.java
@@ -1,5 +1,8 @@
 package demo.boot.graylog;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -13,7 +16,16 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @SpringBootApplication
 @EnableWebMvc
 public class BootGraylogApplication {
+
+  private static Logger log = LoggerFactory.getLogger(BootGraylogApplication.class);
+
   public static void main(String[] args) {
+    log.info("系统启动");
     SpringApplication.run(BootGraylogApplication.class);
+    log.trace("trace日志");
+    log.debug("debug日志");
+    log.info("info日志");
+    log.warn("warn日志");
+    log.error("error日志");
   }
 }
